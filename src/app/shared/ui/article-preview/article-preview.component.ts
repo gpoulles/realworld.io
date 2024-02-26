8cd3c15aea3689c20d281c8dad124c232aefd49d
Gilles Poulles <gpoulles@gmail.com>
2024-02-26 21:22:33 +0100

    #6 replaced favorites html block with conduit-favorites

--- src/app/shared/ui/article-preview/article-preview.component.ts
+++ src/app/shared/ui/article-preview/article-preview.component.ts
@@ -3,11 +3,17 @@ import { Article } from '../../interfaces/article.interface';
 import { RouterLink } from '@angular/router';
 import { MetaDataSnippetComponent } from '../meta-data-snippet/meta-data-snippet.component';
 import { TagsComponent } from '../tags/tags.component';
+import { FavoritesComponent } from '../favorites/favorites.component';
 
 @Component({
   selector: 'conduit-article-preview',
   standalone: true,
-  imports: [RouterLink, MetaDataSnippetComponent, TagsComponent],
+  imports: [
+    RouterLink,
+    MetaDataSnippetComponent,
+    TagsComponent,
+    FavoritesComponent,
+  ],
   templateUrl: './article-preview.component.html',
   styleUrl: './article-preview.component.scss',
 })
