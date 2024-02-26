44417b284b95015ffea628ac5f2fbeda57f5bb36
Gilles Poulles <gpoulles@gmail.com>
2024-02-26 21:19:21 +0100

    #6 replaced tags html block with conduit-tags

--- src/app/shared/ui/article-preview/article-preview.component.ts
+++ src/app/shared/ui/article-preview/article-preview.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Article } from '../../interfaces/article.interface';
 import { RouterLink } from '@angular/router';
 import { MetaDataSnippetComponent } from '../meta-data-snippet/meta-data-snippet.component';
+import { TagsComponent } from '../tags/tags.component';
 
 @Component({
   selector: 'conduit-article-preview',
   standalone: true,
-  imports: [RouterLink, MetaDataSnippetComponent],
+  imports: [RouterLink, MetaDataSnippetComponent, TagsComponent],
   templateUrl: './article-preview.component.html',
   styleUrl: './article-preview.component.scss',
 })
