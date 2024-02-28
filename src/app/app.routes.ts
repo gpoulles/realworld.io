f778ff7072a1f1b887ee3e25d15a602c1e6b0ec2
Gilles Poulles <gpoulles@gmail.com>
2024-02-28 19:39:14 +0100

    #22 create login/register and settings pages
    - created login.component.ts
    - created register.component.ts
    - created settings.component.ts
    - adapted app.routes.ts
    - added menu points to header.component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ArticleComponent } from './pages/article/article.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'article/:slug', component: ArticleComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'settings', component: SettingsComponent },
 ];
