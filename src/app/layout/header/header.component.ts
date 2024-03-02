99153fb9532dd22b6222fe70f6b814eaca79b12f
Gilles Poulles <gpoulles@gmail.com>
2024-03-02 18:09:34 +0100

    #36 added active classes to header.component.html

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UsersService } from '../../shared/services/users.service';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterLink,
+} from '@angular/router';
+import { filter, map, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'conduit-header',
@@ -10,6 +16,41 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-  constructor(public usersService: UsersService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject();
+  currentRoute: string = '';
+  constructor(
+    public usersService: UsersService,
+    private readonly router: Router,
+    private readonly activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        takeUntil(this.destroy$),
+        filter((event) => event instanceof NavigationEnd),
+        map(() => {
+          let route = this.activatedRoute;
+          while (route.firstChild) route = route.firstChild;
+          const path = route.snapshot.url
+            .map((segment) => segment.path)
+            .join('/');
+          return path.split('/')[0];
+        })
+      )
+      .subscribe({
+        next: (firstSegment) => {
+          this.currentRoute = firstSegment;
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
 }
