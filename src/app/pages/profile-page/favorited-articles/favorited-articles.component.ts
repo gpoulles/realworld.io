4348a812b308aa3b1451e55bb58a1c1fc50167ce
gpoulles <gpoulles@gmail.com>
2024-03-07 21:50:00 +0100

    #52 load articles in profile page
    - extented favorited-articles.component.ts by article-base.component.ts
    - load favorite articles
    - adapt html in order to show articles

--- src/app/pages/profile-page/favorited-articles/favorited-articles.component.ts
+++ src/app/pages/profile-page/favorited-articles/favorited-articles.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ArticleListComponent } from '../../../shared/ui/article-list/article-list.component';
+import { PaginationComponent } from '../../../shared/ui/pagination/pagination.component';
+import { ArticlesBaseComponent } from '../../../shared/components/articles-base/articles-base.component';
+import { ArticlesService } from '../../../shared/services/articles.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'conduit-favorited-articles',
   standalone: true,
-  imports: [],
+  imports: [ArticleListComponent, PaginationComponent],
   templateUrl: './favorited-articles.component.html',
-  styleUrl: './favorited-articles.component.scss'
+  styleUrl: './favorited-articles.component.scss',
 })
-export class FavoritedArticlesComponent {
+export class FavoritedArticlesComponent
+  extends ArticlesBaseComponent
+  implements OnDestroy
+{
+  destroy$ = new Subject<void>();
+  constructor(
+    protected override articlesService: ArticlesService,
+    private readonly route: ActivatedRoute
+  ) {
+    super(articlesService);
+    this.route.parent?.data
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(({ profile }) => {
+        this.filters = { ...this.filters, favorited: profile.name };
+        this.loadArticles(this.filters);
+      });
+  }
 
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
